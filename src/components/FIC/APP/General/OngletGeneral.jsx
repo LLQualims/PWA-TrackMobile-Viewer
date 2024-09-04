a9efc5367746025f57bb7deb50ac65e453b790e9
Thibaud Figarol <thibaud.figarol@inoky.com>
2024-09-04 11:34:32 +0200

    #feat-Issue 2 Onglet Général (Appareils)

--- src/components/FIC/APP/General/OngletGeneral.jsx
+++ src/components/FIC/APP/General/OngletGeneral.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import ChampDetailCadre from './ChampDetailCadre';
+import './OngletGeneral.css'
 
 const DATFIC = (props) => {
   const [data, setData] = useState([]);
@@ -9,10 +11,11 @@ const DATFIC = (props) => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`http://192.168.39.68:4500/8.1b/app/appareil/numero`, {
+          const response = await axios.get(`${localStorage.getItem("URLServeur")}/app/appareil/numero`,  {
           headers: {
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJJZCI6ImI2NGQ5N2RkLTE4YTAtNDJkMi1hZTNkLWViM2Q5ZDRlYTQ5MCIsInN1YklkIjoiNzYiLCJzdWIiOiJLUCIsImp0aSI6IjFkZGEyODRmLTZjZTQtNGRlMC04NDEzLTk1NGI2YWI2YWM0MCIsIlByb2ZpbEVRTSI6IjYiLCJQcm9maWxMQUIiOiIxMCIsIm5iZiI6MTcxOTQ5ODE1OSwiZXhwIjoyMDE5NTAxNzU5LCJpYXQiOjE3MTk0OTgxNTksImlzcyI6IklOT0tZIiwiYXVkIjoiUVVBTElNUyJ9.TaF3QoT2AooxmPD6l_vXWFCnKDguU0pGiaGymo4_6mg',
-            NumeroAppareil: `${props.numero}`
+                  Authorization: 'Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJJZCI6IjM5MjQ4MGQwLTMyMTMtNGVlMy1iMTRmLTM1ODQ0NWUyZTE4ZSIsInN1YklkIjoiNTMiLCJzdWIiOiJtZ2lyYXJkIiwianRpIjoiYzNjYjgwOTYtYmE0MC00OWQ4LWI0MTQtZjI4ZWJlZmQ5MjY3IiwiUHJvZmlsRVFNIjoiMTMiLCJQcm9maWxMQUIiOiIxMyIsIm5iZiI6MTcyNTI4MzcyMywiZXhwIjoyNTQ1NzM4MjEyLCJpYXQiOjE3MjUyODM3MjMsImlzcyI6IklOT0tZIiwiYXVkIjoiUVVBTElNUyJ9.a_r1VbmKlGroojvEe9WXilMvKW_sw8Ko1cgsMcbyOTLd8fqkwh9EaIpkPmcc0ZEWuFqat6DYp3ZiCZDkFR8sNA',
+                  NumeroAppareil: `${props.numero}`,
+                  SousEntites: '1'
           }
         });
 
@@ -42,78 +45,33 @@ const DATFIC = (props) => {
   return (
     <div>
      
-    {data.map((item) => (
-    <div>
-      <img src={require(`../../../../assets/Images/STD_Statut${item.idappStatut}-128-1.png`)} className="img-statut" alt="Statut" />
-      <p><strong>ID App Type Etalon:</strong> {item.idappTypeEtalon}</p>
-      <p><strong>ID App Statut:</strong> {item.idappStatut}</p>
-      <p><strong>ID App Famille:</strong> {item.idappFamille}</p>
-      <p><strong>ID App Appareil:</strong> {item.idappAppareil}</p>
-      <p><strong>Numéro Appareil:</strong> {item.numeroAppareil}</p>
-      <p><strong>Désignation Appareil:</strong> {item.designationAppareil}</p>
-      <p><strong>Numéro Série:</strong> {item.numeroSerie}</p>
-      <p><strong>Prix:</strong> {item.prix}</p>
-      <p><strong>Amortissement:</strong> {item.amortissement}</p>
-      <p><strong>Type Amortissement:</strong> {item.typeAmortissement}</p>
-      <p><strong>Date Future Operation:</strong> {item.dateFutureOperation ? new Date(item.dateFutureOperation).toLocaleDateString() : 'N/A'}</p>
-      <p><strong>Remarque:</strong></p>
-      <p><strong>ID Responsable:</strong> {item.idResponsable}</p>
-      <p><strong>Période Garantie:</strong> {item.periodeGarantie ? new Date(item.periodeGarantie).toLocaleDateString() : 'N/A'}</p>
-      <p><strong>Suivi Automatique:</strong> {item.suiviAutomatique}</p>
-      <p><strong>Audit CRE:</strong> {item.auditCRE}</p>
-      <p><strong>Audit DCRE:</strong> {item.auditDCRE ? new Date(item.auditDCRE).toLocaleDateString() : 'N/A'}</p>
-      <p><strong>Audit MOD:</strong> {item.auditMOD}</p>
-      <p><strong>Audit DMOD:</strong> {item.auditDMOD ? new Date(item.auditDMOD).toLocaleDateString() : 'N/A'}</p>
-      <p><strong>Numéro Facture:</strong> {item.numeroFacture}</p>
-      <p><strong>Date Acquisition:</strong> {item.dateAcquisition ? new Date(item.dateAcquisition).toLocaleDateString() : 'N/A'}</p>
-      <p><strong>Marque Appareil:</strong> {item.marqueAppareil}</p>
-      <p><strong>Modèle Appareil:</strong> {item.modeleAppareil}</p>
-      <p><strong>Type Étiquette:</strong> {item.typeEtiquette}</p>
-      <p><strong>ID Env Salle:</strong> {item.idenvSalle}</p>
-      <p><strong>Date Dernière Opération:</strong> {item.dateDerniereOperation ? new Date(item.dateDerniereOperation).toLocaleDateString() : 'N/A'}</p>
-      <p><strong>Enregistrement Archive:</strong> {item.enrARCHIVE}</p>
-      <p><strong>ID Env Service:</strong> {item.idenvService}</p>
-      <p><strong>ID App Domaine:</strong> {item.idappDomaine}</p>
-      <p><strong>Type Appareil:</strong> {item.typeAppareil}</p>
-      <p><strong>ID Indisponibilité:</strong> {item.idIndisponibilite}</p>
-      <p><strong>Date Mise En Service:</strong> {item.dateMiseEnService ? new Date(item.dateMiseEnService).toLocaleDateString() : 'N/A'}</p>
-      <p><strong>Enregistrement Appareil:</strong> {item.enregistrementAppareil}</p>
-      <p><strong>Déclassement:</strong> {item.declassement ? new Date(item.declassement).toLocaleDateString() : 'N/A'}</p>
-      <p><strong>Verrou Operation:</strong> {item.verrouOperation}</p>
-      <p><strong>Numéro Externe Appareil:</strong> {item.numeroExterneAppareil}</p>
-      <p><strong>Date Renouvellement:</strong> {item.dateRenouvellement ? new Date(item.dateRenouvellement).toLocaleDateString() : 'N/A'}</p>
-      <p><strong>Budget Renouvellement:</strong> {item.budgetRenouvellement}</p>
-      <p><strong>ID App Criticité:</strong> {item.idappCriticite}</p>
-      <p><strong>Notification Mail:</strong> {item.notifMail}</p>
-      <p><strong>Info Étiquette:</strong> {item.infoEtiquette}</p>
-      <p><strong>Date Fin Garantie:</strong> {item.dateFinGarantie ? new Date(item.dateFinGarantie).toLocaleDateString() : 'N/A'}</p>
-      <p><strong>Alerte Garantie:</strong> {item.alerteGarantie}</p>
-      <p><strong>Propagation Statut Salle:</strong> {item.propagationStatutSalle}</p>
-      <p><strong>Champ01:</strong> {item.champ01}</p>
-      <p><strong>Champ02:</strong> {item.champ02}</p>
-      <p><strong>Champ03:</strong> {item.champ03}</p>
-      <p><strong>Champ04:</strong> {item.champ04}</p>
-      <p><strong>Champ05:</strong> {item.champ05}</p>
-      <p><strong>Destination Appareil:</strong> {item.destinationAppareil}</p>
-      <p><strong>Numéro Salle:</strong> {item.numeroSalle}</p>
-      <p><strong>Code Service:</strong> {item.codeService}</p>
-      <p><strong>ID Client:</strong> {item.idClient}</p>
-      <p><strong>Enregistrement Verrou:</strong> {item.enrVerrou}</p>
-      <p><strong>Audit Verrou:</strong> {item.auditVERROU}</p>
-      <p><strong>Audit D Verrou:</strong> {item.auditDVERROU ? new Date(item.auditDVERROU).toLocaleDateString() : 'N/A'}</p>
-      <p><strong>ID App Source Énergie:</strong> {item.idappSourceEnergie}</p>
-      <p><strong>ID App Source Prise:</strong> {item.idappSourcePrise}</p>
-      <p><strong>Puissance Électrique:</strong> {item.puissanceElectrique}</p>
-      <p><strong>Localisation Position:</strong> {item.localisationPosition}</p>
-      <p><strong>ID App Type:</strong> {item.idappType}</p>
-      <p><strong>Numéro Immo:</strong> {item.numeroImmo}</p>
-      <p><strong>Numéro Commande:</strong> {item.numeroCommande}</p>
-      <p><strong>Référence:</strong> {item.reference}</p>
-      <p><strong>Firmware:</strong> {item.firmware}</p>
-      <p><strong>Joker:</strong> {item.joker}</p>
+        {data.map((item) => (
+            <div>
+
+                <div id="statut">
+                    <img id="img_statut_fond" src={require(`../../../../assets/Images/STD_Statut${item.idappStatut}-128-1.png`)} alt="Statut" />
+                    <img id="img_statut_etat" className="superpose" src={require(`../../../../assets/Images/APP_Etat7-72-1.png`)} />
+                    <p id="txt_statut_texte" className="superpose">{item.appStatut ? item.appStatut.designationStatut : ""}</p>
+                </div>
+
+                <div id="valeurs_appareil">
+                    <ChampDetailCadre libelle="Famille" valeur={item.appFamille ? item.appFamille.designationFamille : ""} />
+                    <ChampDetailCadre libelle="Numéro Appareil" valeur={item.numeroAppareil} />
+                    <ChampDetailCadre libelle="Désignation Appareil" valeur={item.designationAppareil} />
+                    <ChampDetailCadre libelle="Responsable" valeur={item.responsable ? item.responsable.prenomPersonnel + " " + item.responsable.nomPersonnel : ""} />
+                    <ChampDetailCadre libelle="Localisation" valeur={item.envSalle ? item.envSalle.numeroSalle + " " + item.envSalle.designationSalle : ""} />
+                    <ChampDetailCadre libelle="Prochaine Opération" valeur={item.dateFutureOperation ? new Date(item.dateFutureOperation).toLocaleDateString() : 'N/A'} />
+                    <ChampDetailCadre libelle="Type" valeur={item.appType ? item.appType.designationType : ""} />
+                    <ChampDetailCadre libelle="Date Acquisition" valeur={item.dateAcquisition ? new Date(item.dateAcquisition).toLocaleDateString() : 'N/A'} />
+                    <ChampDetailCadre libelle="Date Mise En Service" valeur={item.dateMiseEnService ? new Date(item.dateMiseEnService).toLocaleDateString() : 'N/A'} />
+                    <ChampDetailCadre libelle="Marque Appareil" valeur={item.marqueAppareil} />
+                    <ChampDetailCadre libelle="Numéro Série" valeur={item.numeroSerie} />
+                    <ChampDetailCadre libelle="Info Étiquette" valeur={item.infoEtiquette} />
+                    <ChampDetailCadre libelle="Remarque" valeur={item.remarqueHTML} />
+                </div>
+            </div>
+        ))}
     </div>
-    ))}
-  </div>
 );
 };
 
