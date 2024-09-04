c5c4a0e4e1e66c7e34441af0ed524c49d8517ae8
Thibaud <143626821+Thibaud-Qualims@users.noreply.github.com>
2024-09-04 11:54:18 +0200

    Appareil onglet général (#5)
    * #feat-Issue 2
    Onglet Général (Appareils)
    
    * #feat-Issue 2
    Onglet Général (Appareils)
    
    * rebase sur main

---
+++ src/components/FIC/APP/General/ChampDetailCadre.jsx
@@ -0,0 +1,16 @@
+﻿import React, { useEffect, useState } from 'react';
+import TextField from '@mui/material/TextField';
+import './ChampDetailCadre.css';
+
+export default function ChampDetailCadre({ libelle, valeur }) {
+
+    return (
+        <div className="divcadre">
+            <TextField
+                className="textfieldcadre"
+                inputProps={{ readOnly: true }}
+                label={libelle}
+                defaultValue={valeur} />
+        </div>
+    );
+};
