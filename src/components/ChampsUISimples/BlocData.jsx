c5643dea225635bf2cb80123aa495683503aa755
Thibaud Figarol <thibaud.figarol@inoky.com>
2024-09-06 12:09:21 +0200

    refactor: ChampsUISimples Factorisation des champs UI dans un dossier ChampsUISimples dans components

---
+++ src/components/ChampsUISimples/BlocData.jsx
@@ -0,0 +1,43 @@
+﻿import './ChampsUISimples.css';
+import { useNavigate } from 'react-router-dom';
+
+export default function BlocData({ ligne1, ligne2, image, altImage, urlDestination }) {
+
+    const navigate = useNavigate();
+
+    function Image() {
+        if (image === undefined) {
+            return null;
+        }
+
+        return <img src={image} alt={altImage} className="blocdata_image" />;
+    }
+
+    function Texte() {
+        return (
+            <div className='blocdata_div'>
+                <p className='blocdata_ligne1'>{ligne1}</p>
+                <p className='blocdata_ligne2'>{ligne2}</p>
+            </div>
+        );
+    }
+    function Bouton() {
+        if (urlDestination === undefined) {
+            return (
+                <button className='blocdata_bouton' type="button">
+                    <Image />
+                    <Texte />
+                </button>
+            );
+        }
+
+        return (
+            <button className='blocdata_bouton' type="button" onClick={() => navigate(`${urlDestination}`)} >
+                <Image />
+                <Texte />
+            </button>
+        );
+    }
+
+    return <Bouton/>
+};
--- src/components/FIC/APP/General/OngletGeneral.jsx
+++ src/components/FIC/APP/General/OngletGeneral.jsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import ChampDetailCadre from './ChampDetailCadre';
-import './OngletGeneral.css';
-import Box from '@mui/material/Box';
-import CircularProgress from '@mui/material/CircularProgress';
+import TextFieldReadonly from '../../../ChampsUISimples/TextFieldReadonly';
+import CircularProgress from '../../../ChampsUISimples/CircularProgress';
+import './OngletGeneral.css'
 
 const DATFIC = (props) => {
   const [data, setData] = useState([]);
@@ -36,44 +35,45 @@ const DATFIC = (props) => {
     fetchData();
   },[]);
 
+  if (loading) {
+    return <div><CircularProgress/></div>;
+  }
+
   if (error) {
     return <div>Error: {error.message}</div>;
   }
 
   return (
-    <div className='tab'>
-      <p className='titreonglet'>INFORMATIONS SUR L'APPAREIL</p>
+    <div>
+     
+        {data.map((item) => (
+            <div>
 
-      {loading ? (
-        <Box sx={{ marginTop: "10%" }}>
-          <CircularProgress />
-        </Box>
-      ) : (
-        data.map((item) => (
-            <div className='infosgeneral'>
-              
                 <div id="statut">
                     <img id="img_statut_fond" src={require(`../../../../assets/Images/STD_Statut${item.idappStatut}-128-1.png`)} alt="Statut" />
                     <img id="img_statut_etat" className="superpose" src={require(`../../../../assets/Images/APP_Etat7-72-1.png`)} />
                     <p id="txt_statut_texte" className="superpose">{item.appStatut ? item.appStatut.designationStatut : ""}</p>
                 </div>
-                    <ChampDetailCadre libelle="Famille" valeur={item.appFamille ? item.appFamille.designationFamille : ""} />
-                    <ChampDetailCadre libelle="Numéro Appareil" valeur={item.numeroAppareil} />
-                    <ChampDetailCadre libelle="Désignation Appareil" valeur={item.designationAppareil} />
-                    <ChampDetailCadre libelle="Responsable" valeur={item.responsable ? item.responsable.prenomPersonnel + " " + item.responsable.nomPersonnel : ""} />
-                    <ChampDetailCadre libelle="Localisation" valeur={item.envSalle ? item.envSalle.numeroSalle + " " + item.envSalle.designationSalle : ""} />
-                    <ChampDetailCadre libelle="Prochaine Opération" valeur={item.dateFutureOperation ? new Date(item.dateFutureOperation).toLocaleDateString() : 'N/A'} />
-                    <ChampDetailCadre libelle="Type" valeur={item.appType ? item.appType.designationType : ""} />
-                    <ChampDetailCadre libelle="Date Acquisition" valeur={item.dateAcquisition ? new Date(item.dateAcquisition).toLocaleDateString() : 'N/A'} />
-                    <ChampDetailCadre libelle="Date Mise En Service" valeur={item.dateMiseEnService ? new Date(item.dateMiseEnService).toLocaleDateString() : 'N/A'} />
-                    <ChampDetailCadre libelle="Marque Appareil" valeur={item.marqueAppareil} />
-                    <ChampDetailCadre libelle="Numéro Série" valeur={item.numeroSerie} />
-                    <ChampDetailCadre libelle="Info Étiquette" valeur={item.infoEtiquette} />
-                    <ChampDetailCadre libelle="Remarque" valeur={item.remarqueHTML} />
+
+                <div>
+                    <TextFieldReadonly libelle="Famille" valeur={item.appFamille ? item.appFamille.designationFamille : ""} />
+                    <TextFieldReadonly libelle="Numéro Appareil" valeur={item.numeroAppareil} />
+                    <TextFieldReadonly libelle="Désignation Appareil" valeur={item.designationAppareil} />
+                    <TextFieldReadonly libelle="Responsable" valeur={item.responsable ? item.responsable.prenomPersonnel + " " + item.responsable.nomPersonnel : ""} />
+                    <TextFieldReadonly libelle="Localisation" valeur={item.envSalle ? item.envSalle.numeroSalle + " " + item.envSalle.designationSalle : ""} />
+                    <TextFieldReadonly libelle="Prochaine Opération" valeur={item.dateFutureOperation ? new Date(item.dateFutureOperation).toLocaleDateString() : 'N/A'} />
+                    <TextFieldReadonly libelle="Type" valeur={item.appType ? item.appType.designationType : ""} />
+                    <TextFieldReadonly libelle="Date Acquisition" valeur={item.dateAcquisition ? new Date(item.dateAcquisition).toLocaleDateString() : 'N/A'} />
+                    <TextFieldReadonly libelle="Date Mise En Service" valeur={item.dateMiseEnService ? new Date(item.dateMiseEnService).toLocaleDateString() : 'N/A'} />
+                    <TextFieldReadonly libelle="Marque Appareil" valeur={item.marqueAppareil} />
+                    <TextFieldReadonly libelle="Numéro Série" valeur={item.numeroSerie} />
+                    <TextFieldReadonly libelle="Info Étiquette" valeur={item.infoEtiquette} />
+                    <TextFieldReadonly libelle="Remarque" valeur={item.remarqueHTML} />
+                </div>
             </div>
-          ))
-        )}
+        ))}
     </div>
-      )
-    };
+);
+};
+
 export default DATFIC;
