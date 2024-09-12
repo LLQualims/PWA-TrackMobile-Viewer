927f319fced0a184cc524857f2f9786330876419
Thibaud Figarol <thibaud.figarol@inoky.com>
2024-09-12 10:26:55 +0200

    Issue #14 - Onglet Caracteristiques LARCOFIC

--- src/pages/LARCOFIC/LARCOFIC.jsx
+++ src/pages/LARCOFIC/LARCOFIC.jsx
@@ -1,8 +1,9 @@
 ﻿import { useParams } from 'react-router-dom';
 import LARCOFIC_OngletHistorique from "../../components/FIC/LAR/Historique/OngletHistorique";
+import LARCOFIC_OngletCaracteristiques from '../../components/FIC/LAR/Caracteristiques/OngletCaracteristiques';
 
 export default function LARCOFIC() {
     const { idlarConditionnement } = useParams();
 
-    return <LARCOFIC_OngletHistorique id={idlarConditionnement} />;
+    return <LARCOFIC_OngletCaracteristiques id={idlarConditionnement} />;
 }
