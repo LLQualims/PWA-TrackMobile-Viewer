0665b3b3d9481ca843e9384073ee88348886aaf6
LLQualims <luka.lai@qualims.com>
2024-09-16 16:26:12 +0200

    onglet corrections

--- src/components/FIC/APP/General/OngletGeneral.jsx
+++ src/components/FIC/APP/General/OngletGeneral.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+﻿import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import TextFieldReadonly from '../../../ChampsUISimples/TextFieldReadonly';
 import CircularProgress from '../../../ChampsUISimples/CircularProgress';
